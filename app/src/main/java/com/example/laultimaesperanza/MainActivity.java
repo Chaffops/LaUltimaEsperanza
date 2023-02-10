8e116f03511a05ca45ce9bf7a59abaa5c2751266
Chaff <xabiervilla@gmail.com>
2023-02-10 18:04:51 +0100

    funcionan los fragments

--- app/src/main/java/com/example/laultimaesperanza/MainActivity.java
+++ app/src/main/java/com/example/laultimaesperanza/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.laultimaesperanza;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.laultimaesperanza.dialogos.DialogoFragment;
+import com.example.laultimaesperanza.pantallasYvistas.FragmentPrincipal;
+import com.example.laultimaesperanza.pantallasYvistas.FragmentScore;
+import com.example.laultimaesperanza.pantallasYvistas.FragmentTienda;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fManager = getSupportFragmentManager();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,17 +24,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void empezarJuego(View vista){
+    public void empezarJuego(View vista) {
 
-        Intent iJuego=new Intent(this,PantallaJuego.class);
+        Intent iJuego = new Intent(this, PantallaJuego.class);
         startActivity(iJuego);
 
     }
 
-    public void ajustar(View v){
-        DialogoFragment dialogo=new DialogoFragment();
-        dialogo.show(getSupportFragmentManager(),"DialogoFragment");
+    public void ajustar(View v) {
+        DialogoFragment dialogo = new DialogoFragment();
+        dialogo.show(getSupportFragmentManager(), "DialogoFragment");
 
     }
+
+    public void irPrincipal(View vista) {
+
+        fManager.beginTransaction()
+                .replace(R.id.ContenedorFragments, FragmentPrincipal.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("")
+                .commit();
+
+    }
+
+
+    public void irTienda(View vista) {
+        fManager.beginTransaction()
+                .replace(R.id.ContenedorFragments, FragmentTienda.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("")
+                .commit();
+
+
+    }
+
+    public void irScoreBoard(View vista) {
+        fManager.beginTransaction()
+                .replace(R.id.ContenedorFragments, FragmentScore.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("")
+                .commit();
+
+
+    }
+
 
 }
